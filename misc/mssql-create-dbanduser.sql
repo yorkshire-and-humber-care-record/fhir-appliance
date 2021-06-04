87dc70bff432a426996975289d6b731b74f03042
Rob Organ <rob.organ@synanetics.com>
2021-06-04 16:49:21 +0100

    folder structure, fake certs, fake JWT private/public key
    folder structure updated (jwt, ssl, pix) and fake CA signed certs added; fake jwt pk and pub key added; docker-compose file updated to include volume mount comments for windows containers; docker-compose files updated with pix, jwt volumen mounts; env files modified to point to fake certs;

--- misc/mssql-create-dbanduser.sql
+++ misc/mssql-create-dbanduser.sql
@@ -1,17 +1,22 @@
 USE [master];
 GO
-CREATE LOGIN fhir_appliance_db_user 
-    WITH PASSWORD    = N'[YOUR_PASSWORD_HERE]',
-    CHECK_POLICY     = OFF,
-    CHECK_EXPIRATION = OFF;
+  CREATE LOGIN fhir_appliance_db_user WITH PASSWORD = N'[YOUR_PASSWORD_HERE]',
+  /* THIS PASSWORD MUST BE USED IN THE DATABASE CONNECTION STRING SECTION (line 44 in .env) */
+  CHECK_POLICY = OFF,
+  CHECK_EXPIRATION = OFF;
 GO
-CREATE DATABASE fhirstore;
+  CREATE DATABASE fhirstore;
 GO
-USE fhirstore;
+  USE fhirstore;
 GO
-IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'fhir_appliance_db_user')
-BEGIN
-    CREATE USER [fhir_appliance_db_user] FOR LOGIN [fhir_appliance_db_user]
-    EXEC sp_addrolemember N'db_owner', N'fhir_appliance_db_user'
+  IF NOT EXISTS (
+    SELECT
+      *
+    FROM
+      sys.database_principals
+    WHERE
+      name = N'fhir_appliance_db_user'
+  ) BEGIN CREATE USER [fhir_appliance_db_user] FOR LOGIN [fhir_appliance_db_user] EXEC sp_addrolemember N'db_owner',
+  N'fhir_appliance_db_user'
 END;
 GO
