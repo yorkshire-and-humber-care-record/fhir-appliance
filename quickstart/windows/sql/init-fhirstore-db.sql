191a227a84b49012034d6d6fd4151431e751dce7
Oliver Musgrove-Wethey <oliver.musgrove-wethey@clearreview.com>
2023-07-07 10:52:26 +0100

    Build MSSQL Instance Rather Than Relying On PreBuilt Image
    This means when switching between different versions of the windows OS, we dont need to mantain different versions
    of this image built on different OS's. As this is just a sample project to get people up and running having a
    fixed image is not required

---
+++ quickstart/windows/sql/init-fhirstore-db.sql
@@ -0,0 +1,20 @@
+/* CREATE THE FHIRSTORE LOGIN (iamnode) */
+USE [master];
+GO
+CREATE LOGIN iamnode
+    WITH PASSWORD    = N'Pa55w0rd1!',
+    CHECK_POLICY     = OFF,
+    CHECK_EXPIRATION = OFF;
+GO
+/* CREATE THE fhirstore DB */
+CREATE DATABASE fhirstore;
+GO
+USE fhirstore;
+GO
+/* CREATE THE DBO iamnode USER AND LINKE TO THE iamnode LOGIN */
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'iamnode')
+BEGIN
+    CREATE USER [iamnode] FOR LOGIN [iamnode]
+    EXEC sp_addrolemember N'db_owner', N'iamnode'
+END;
+GO
